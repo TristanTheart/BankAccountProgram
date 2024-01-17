13857d3d3eeeea3b913735ff8fe777ebdbefc526
wishfully28 <thewishfulthinker28@gmail.com>
2024-01-17 22:00:06 +0200

    Example BankAccountProgram

--- app/src/main/java/com/example/bankaccountprogram/BankMain.kt
+++ app/src/main/java/com/example/bankaccountprogram/BankMain.kt
@@ -1,2 +1,26 @@
 package com.example.bankaccountprogram
 
+fun main() {
+
+    val tristanBankAccount = BankAccount(accountHolder = "Tristan Theart", balance = 1338.20)
+    val sarahBankAccount = BankAccount(accountHolder = "Sarah", balance = 0.0)
+
+    tristanBankAccount.deposit(amount = 200.00)
+    tristanBankAccount.withdraw(amount = 1200.00)
+    tristanBankAccount.deposit(amount = 3000.00)
+    tristanBankAccount.deposit(amount = 2000.00)
+    tristanBankAccount.withdraw(amount = 3333.15)
+
+    tristanBankAccount.displayTransactionHistory()
+    println("${tristanBankAccount.accountHolder}'s balance is ${tristanBankAccount.acctBalance()}")
+    println()
+
+
+    sarahBankAccount.deposit(amount = 100.00)
+    sarahBankAccount.withdraw(amount = 10.00)
+    sarahBankAccount.deposit(amount = 300.00)
+
+    sarahBankAccount.displayTransactionHistory()
+    println("${sarahBankAccount.accountHolder}'s balance is ${sarahBankAccount.acctBalance()}")
+    println()
+}
